e5e9f0ab6840ec30a00e79c8675e33ca078bf265
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-03-18 23:08:57 +0200

    fix validator

--- helpers/contactValidator.js
+++ helpers/contactValidator.js
@@ -1,15 +1,16 @@
 const Joi = require("joi");
 
-const contactValidator = (data) => {
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(2).max(30).required(),
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "ua"] } })
-      .required(),
-    phone: Joi.string().required(),
-  });
-
-  return schema.validate(data);
-};
+const contactValidator = (data) =>
+  Joi.object()
+    .options({ abortEarly: false })
+    .keys({
+      name: Joi.string().min(2).max(30).required(),
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "ua"] } })
+        .required(),
+      phone: Joi.string().required(),
+      favorite: Joi.boolean(),
+    })
+    .validate(data);
 
 module.exports = contactValidator;
--- middlewares/contactMiddleware.js
+++ middlewares/contactMiddleware.js
@@ -8,13 +8,14 @@ exports.checkBody = catchAsync(async (req, res, next) => {
 
   const { error, value } = contactValidator(req.body);
 
+  const missingFields = error.details.map((item) => item.context.key);
+  const fields =
+    missingFields.length === 1
+      ? missingFields.join("")
+      : missingFields.join(" & ");
+
   if (error)
-    return next(
-      new AppError(
-        400,
-        `missing required ${error.details[0].context?.key} field`
-      )
-    );
+    return next(new AppError(400, `missing required ${fields} field(s)`));
 
   const nameExists = await Contact.exists({ name: value.name });
   const emailExists = await Contact.exists({ email: value.email });
@@ -35,11 +36,13 @@ exports.checkBody = catchAsync(async (req, res, next) => {
 exports.checkContact = catchAsync(async (req, res, next) => {
   const { contactId } = req.params;
 
-  const isIdValid = await Types.ObjectId.isValid(contactId);
+  const isIdValid = Types.ObjectId.isValid(contactId);
 
   if (!isIdValid) return next(new AppError(404, "Not found"));
 
-  const contactExists = Contact.exists({ _id: contactId });
+  const contactExists = await Contact.exists({ _id: contactId });
+
+  console.log(contactExists);
 
   if (!contactExists) return next(new AppError(404, "Not found"));
 
