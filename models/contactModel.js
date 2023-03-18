390f398424ed252bec2e5dcc10f6e7c50595fa6d
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-03-18 23:09:14 +0200

    add model

---
+++ models/contactModel.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+    unique: [true, "Contact with this name already exists"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: [true, "Contact with this email already exists"],
+    trim: true,
+    lowercase: true,
+  },
+  phone: {
+    type: String,
+    unique: [true, "Contact with this phone already exists"],
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = model("Contact", contactSchema);
+
+module.exports = Contact;
